c95d79293c9abe1bac363ab6a6f1243da6cf006c
KristianAlm <kristian.alm83@gmail.com>
2015-02-04 10:07:22 +0100

    Initial check-in of module Porter

---
+++ Client/Porter/RegisterUser.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+namespace Porter
+{
+	public partial class RegisterUser : ContentPage
+	{
+		public RegisterUser ()
+		{
+			this.LoadFromXaml(typeof(RegisterUser));
+		}
+
+		public void SaveName(object sender, EventArgs e)
+		{
+			var labelSaved = this.FindByName<Label> ("Saved");
+			var entryBox = this.FindByName<Entry> ("NameEntry");
+
+			labelSaved.Text = "Du er nå lagret som " + entryBox.Text;
+
+
+		}
+	}
+}
+
